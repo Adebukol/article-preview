2b41ea2708c274523f694272979aeb1bc2cac733
Adebukol <adebukolaomowole@gmail.com>
2023-07-09 00:29:01 +0100

    share button updated

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-function showDiv(){
-  document.getElementById('show-el').textContent.style.display ='block';
-}
+// function showDiv(){
+const shareBtn = document.getElementById("share-btn")
 
-
- 
-
-            
+ shareBtn.addEventListener("click",e)     
+ function showDiv(){
+  if(document.querySelector(".tooltip").style.visibility= "hidden"){
+   document.querySelector(".tooltip").style.visibility= "visible"
+   shareBtn.style.background= "hsl(217, 19%, 35%)";
+   shareBtn.src= "images/icon-sharewhite.svg";
+  }
+ }
